7bd60d82b421072f7c9dbd7d7d1679ff0abde7e9
Beto Schneider <roberto.schneider@gmail.com>
2026-02-05 19:09:32 -0300

    centralizando imagem

--- app.py
+++ app.py
@@ -1,6 +1,5 @@
 import streamlit as st
 import qrcode
-# from PIL import Image
 import io
 from datetime import datetime
 
@@ -18,23 +17,41 @@ def main():
 
     if valor:
         # Gerar QR Code
-        qr = qrcode.QRCode(version=1, box_size=20, border=5)
+        qr = qrcode.QRCode(version=1, box_size=10, border=5)
         qr.add_data(valor)
         qr.make(fit=True)
         img = qr.make_image(fill_color="black", back_color="white")
 
-        # Plotar imagem
-        st.image(img.get_image())
+        # Criar colunas para centralizar
+        col1, col2, col3 = st.columns([1, 2, 1])
 
-        # Download
-        buf = io.BytesIO()
-        img.save(buf, format="PNG")
-        date = datetime.now().strftime("%Y-%m-%d_%H-%M-%S")
-        st.download_button("Baixar QR Code", buf.getvalue(), f"qrcode_{date}.png", "image/png", icon=":material/download:")
+        with col2:
+            # Plotar imagem
+            st.image(img.get_image(), width="stretch")
+
+            # Download
+            buf = io.BytesIO()
+            img.save(buf, format="PNG")
+            date = datetime.now().strftime("%Y-%m-%d_%H-%M-%S")
+            st.download_button(
+                "Baixar QR Code", 
+                buf.getvalue(), 
+                f"qrcode_{date}.png", 
+                "image/png", 
+                icon=":material/download:",
+                width="stretch"
+            )
 
     st.markdown("---")
-    st.markdown("Desenvolvido por [betoschneider.com](https://betoschneider.com)")
-    st.markdown("Código-fonte disponível no [GitHub](https://github.com/betoschneider/qr-code-streamlit)")
+    st.markdown(
+        """
+        <div style='text-align: center;'>
+            Desenvolvido por <a href='https://betoschneider.com' target='_blank'>betoschneider.com</a> | 
+            Disponível no <a href='https://github.com/betoschneider/qr-code-streamlit' target='_blank'>GitHub</a>
+        </div>
+        """, 
+        unsafe_allow_html=True
+    )
 
 if __name__ == "__main__":
     main()
